08dec8269d6d74bb5a9d0517ce4974a5c1469455
phạm hồng định <duiga2611@gmail.com>
2024-02-29 17:38:47 +0700

    auth login

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ import routerBindings, {
 import { App as AntdApp } from "antd";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
+import {Home,ForgotPassword,Login,Register} from './pages';
 
 
 function App() {
@@ -40,6 +40,10 @@ function App() {
               >
                 <Routes>
                   <Route index element={<WelcomePage />} />
+                  <Route index element={<Home />} />
+                  <Route path="/register" element={<Register />} />
+                  <Route path="/login" element={<Login />} />
+                  <Route path="/forgot-password" element={<ForgotPassword />} />
                 </Routes>
                 <RefineKbar />
                 <UnsavedChangesNotifier />
